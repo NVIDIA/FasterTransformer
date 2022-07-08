a9a5817ec446e98ac752d3841f2cdd711bd7e836
Erich Elsen <erich@adept.ai>
2022-07-08 23:53:45 +0000

    add launch script, fix import error in gpt_example.py

---
+++ launch_script.sh
@@ -0,0 +1,12 @@
+python3 examples/pytorch/gpt/gpt_example.py \
+	--layer_num 32 \
+	--head_num 12 \
+        --size_per_head 128 \
+        --ckpt_path "/checkpoint/1-gpu/" \
+        --vocab_file "/vocabularies/gpt2-vocab.json" \
+        --merges_file "/vocabularies/gpt2-merges.txt" \
+	--max_seq_len 2048 \
+	--data_type "bf16" \
+	--sample_input_file "inputs.txt" \
+	--sample_output_file "outputs.txt" \
+	--lib_path "build/lib/libth_gpt.so" \
