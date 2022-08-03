8f22bf87381a938e865a584aea0ac19dfc197332
Thomas Wood <odell.wood@gmail.com>
2022-08-03 09:25:55 -0700

    Update DecoderSelfAttentionLayer.cc
    Need to allow head_size to be 80 for Saleforce CodeGen GPTJ based model.

--- src/fastertransformer/layers/attention_layers/DecoderSelfAttentionLayer.cc
+++ src/fastertransformer/layers/attention_layers/DecoderSelfAttentionLayer.cc
@@ -228,8 +228,9 @@ DecoderSelfAttentionLayer<T>::DecoderSelfAttentionLayer(size_t max_batch_size,
     q_scaling_(q_scaling),
     int8_mode_(int8_mode)
 {
-    FT_CHECK(size_per_head_ == 32 || size_per_head_ == 64 || size_per_head_ == 96 || size_per_head_ == 128
-             || size_per_head_ == 160 || size_per_head_ == 192 || size_per_head_ == 224 || size_per_head_ == 256);
+    FT_CHECK(size_per_head_ == 32 || size_per_head_ == 64 || size_per_head_ == 80 || size_per_head_ == 96 
+             || size_per_head_ == 128 || size_per_head_ == 160 || size_per_head_ == 192 || size_per_head_ == 224
+             || size_per_head_ == 256);
 }
 
 template<typename T>
