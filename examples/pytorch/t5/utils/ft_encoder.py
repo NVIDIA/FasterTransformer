fff55e73445199a0b187bcbc262645f6a750d11c
Qing Lan <lanking520@live.com>
2023-02-08 00:33:26 -0600

    support gated_activation on flant5 series model (#441)

--- examples/pytorch/t5/utils/ft_encoder.py
+++ examples/pytorch/t5/utils/ft_encoder.py
@@ -104,11 +104,19 @@ class FTT5EncoderWeight(object):
         t = torch.stack([encoder_weight_dict["encoder.block.{}.layer.1.layer_norm.weight".format(i)]
                         for i in range(start_layer, end_layer)], 0).contiguous().cuda()
         self.w.append(t)
-        t = torch.stack([encoder_weight_dict["encoder.block.{}.layer.1.DenseReluDense.wi.weight".format(i)]
-                        for i in range(start_layer, end_layer)], 0).contiguous().cuda()
-        self.w.append(t.split(t.shape[-1] // self.tensor_para_size, dim=-1)[self.tensor_para_rank].contiguous())
-        # add empty wi2
-        self.w.append(torch.empty((1, 1), dtype=torch_weight_dtype).contiguous().cuda())
+        if self.use_gated_activation:
+            t = torch.stack([encoder_weight_dict["encoder.block.{}.layer.1.DenseReluDense.wi_0.weight".format(i)]
+                            for i in range(start_layer, end_layer)], 0).contiguous().cuda()
+            self.w.append(t.split(t.shape[-1] // self.tensor_para_size, dim=-1)[self.tensor_para_rank].contiguous())
+            t = torch.stack([encoder_weight_dict["encoder.block.{}.layer.1.DenseReluDense.wi_1.weight".format(i)]
+                             for i in range(start_layer, end_layer)], 0).contiguous().cuda()
+            self.w.append(t.split(t.shape[-1] // self.tensor_para_size, dim=-1)[self.tensor_para_rank].contiguous())
+        else:
+            t = torch.stack([encoder_weight_dict["encoder.block.{}.layer.1.DenseReluDense.wi.weight".format(i)]
+                            for i in range(start_layer, end_layer)], 0).contiguous().cuda()
+            self.w.append(t.split(t.shape[-1] // self.tensor_para_size, dim=-1)[self.tensor_para_rank].contiguous())
+            # add empty wi2
+            self.w.append(torch.empty((1, 1), dtype=torch_weight_dtype).contiguous().cuda())
         t = torch.stack([encoder_weight_dict["encoder.block.{}.layer.1.DenseReluDense.wo.weight".format(i)]
                         for i in range(start_layer, end_layer)], 0).contiguous().cuda()
         self.w.append(t.split(t.shape[1] // self.tensor_para_size, dim=1)[self.tensor_para_rank].contiguous())
